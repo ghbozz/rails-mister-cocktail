34aff9520408db66a6e60e797982c2c7b151b2c7
romain sanson <romainsanson.gm@gmail.com>
2018-08-16 22:29:51 +0200

    Mister Cocktail Front Update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,6 +30,6 @@ class CocktailsController < ApplicationController
   private
 
  def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredients, :doses)
+    params.require(:cocktail).permit(:name, :ingredients, :doses, :description, :image_url)
   end
 end
