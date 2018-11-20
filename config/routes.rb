c86b3ae31df05e530f1a64afad8ba886fdb70511
romain sanson <romainsanson.gm@gmail.com>
2018-11-20 15:19:47 +0100

    mrCocktail update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # get 'cocktails/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "cocktails#index"
   resources :doses, only: [:destroy]
   resources :cocktails do
     resources :doses, only: [:new, :create]
