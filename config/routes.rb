fc4c0ca32deed4d0567833508d40ce55e4c95aaa
romain sanson <romainsanson.gm@gmail.com>
2018-11-21 08:32:35 +0100

    routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # get 'cocktails/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "cocktails#index"
+
   resources :doses, only: [:destroy]
+
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
