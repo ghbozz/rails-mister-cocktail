024ac825bdbd40179e29a0ba646a7f0f817672e7
romain sanson <romainsanson.gm@gmail.com>
2018-11-24 01:26:50 +0100

    front on cocktails show

--- app/javascript/components/reviews.js
+++ app/javascript/components/reviews.js
@@ -1,16 +1,16 @@
 
-if (document.getElementById('review_content')) {
-  const field = document.getElementById('review_content')
-  const btn = document.getElementById('create-review-btn')
+// if (document.getElementById('review_content')) {
+//   const field = document.getElementById('review_content')
+//   const btn = document.getElementById('create-review-btn')
 
-  btn.setAttribute('disabled', true)
+//   btn.setAttribute('disabled', true)
 
-  field.addEventListener('keyup', event => {
-    if (field.value != "" ) {
-      btn.removeAttribute('disabled', true)
-      console.log('io')
-    } else {
-      btn.setAttribute('disabled', true)
-    }
-  })
-}
+//   field.addEventListener('keyup', event => {
+//     if (field.value != "" ) {
+//       btn.removeAttribute('disabled', true)
+//       console.log('io')
+//     } else {
+//       btn.setAttribute('disabled', true)
+//     }
+//   })
+// }
